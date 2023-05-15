e7ea6c555e3bfb65d6d677fccf1dcb65d2ee704a
Vrd Perera <vrdperera@gmail.com>
2023-05-15 22:34:46 +0530

    change css

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,9 +2,19 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col justify-center p-12 text-3xl text-black sm:p-6 md:p-8">
-      <div className="relative h-12 w-12 object-cover sm:mx-auto sm:w-full sm:max-w-md">
-        <Image alt="Logo" src="/vercel.svg" fill className="mx-auto w-auto" />
+    <div className="flex min-h-screen flex-col justify-center bg-zinc-900 p-12 text-3xl text-black sm:p-6 md:p-8">
+      <div className="">
+        <div className="relative h-[96px] w-[96px] object-cover sm:mx-auto sm:w-full sm:max-w-md">
+          <Image
+            alt="Logo"
+            src="/images/logo.svg"
+            className="mx-auto w-auto"
+            fill
+          />
+        </div>
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-zinc-50">
+          Sign in to your account
+        </h2>
       </div>
     </div>
   );
